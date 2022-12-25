d05c198a635d61ab8f367313f0f456429a43294a
Ivanov Alexandru <alexandruivanov8@gmail.com>
2022-12-26 01:56:18 +0200

    login + register page

---
+++ src/main/java/com/example/bdtema/models/IngredientModel.java
@@ -0,0 +1,17 @@
+package com.example.bdtema.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class IngredientModel {
+
+    public String id;
+    public String name;
+
+}
